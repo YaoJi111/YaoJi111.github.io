7fa9713beb2005537ebeb6313d599b7b7eebec70
YaoJi111 <chenhaoze12589@gmail.com>
2025-03-06 20:45:44 +0800

    YaoJi.github.io

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,25 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `欢迎来到我的网站, ${myName}`;
+  }
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `欢迎来到我的网站, ${storedName}`;
+  }
+  myButton.onclick = function () {
+    setUserName();
+  };
+  function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `欢迎来到我的网站, ${myName}`;
+    }
+  }
